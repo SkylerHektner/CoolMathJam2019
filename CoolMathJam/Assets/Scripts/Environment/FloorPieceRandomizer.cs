a94294cde6561324861327a24aa8faa64fba5153
Skyler Hektner <skylerhektner@gmail.com>
2019-02-09 02:04:45 -0700

    Experimenting with visuals and added a randomized floor tile prefab

---
+++ CoolMathJam/Assets/Scripts/Environment/FloorPieceRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FloorPieceRandomizer : MonoBehaviour
+{
+    public Mesh[] TopMeshes;
+    public Mesh[] FrontMeshes;
+    public MeshFilter TopMeshFilter;
+    public MeshFilter FrontMeshFilter;
+
+    void Start()
+    {
+        int TopMeshIndex = Random.Range(0, TopMeshes.Length);
+        int FrontMeshIndex = Random.Range(0, FrontMeshes.Length);
+        TopMeshFilter.mesh = TopMeshes[TopMeshIndex];
+        FrontMeshFilter.mesh = FrontMeshes[FrontMeshIndex];
+    }
+}
