d79e21f41192f12ae299104ffba39b5499e2643f
Skyler Hektner <skylerhektner@gmail.com>
2019-02-09 00:17:27 -0700

    First commit, movement working sorta. Starting on cam controller

---
+++ CoolMathJam/Assets/Scripts/CamController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float FollowOffsetZ = 10f;
+    public float FollowOffsetY = 2f;
+    public float LerpFactor = 5f;
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ CoolMathJam/Assets/Scripts/SkullMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SkullMovement : MonoBehaviour
+{
+    public float RollPower = 5000f;
+    public float JumpPower = 10f;
+    public float FallAccelerationFactor = 2f;
+
+    private Rigidbody rb;
+    private bool jumped = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * RollPower, ForceMode.Acceleration);
+
+        if (!jumped && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+            jumped = true;
+        }
+        else if (jumped)
+        {
+            float holdJumpFactor = 1f;
+            if (Input.GetButton("Jump"))
+                holdJumpFactor = 0.5f;
+            rb.AddForce(Vector3.down * FallAccelerationFactor * Time.deltaTime * holdJumpFactor, ForceMode.Acceleration);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            jumped = false;
+        }
+    }
+}
