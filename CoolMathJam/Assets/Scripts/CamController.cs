3a23b76cdcdac86507ff9e449f03f6cf50c1489e
Skyler Hektner <skylerhektner@gmail.com>
2019-02-09 00:21:35 -0700

    Cam controller working

--- CoolMathJam/Assets/Scripts/CamController.cs
+++ CoolMathJam/Assets/Scripts/CamController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CamController : MonoBehaviour
 {
     public float FollowOffsetZ = 10f;
     public float FollowOffsetY = 2f;
     public float LerpFactor = 5f;
+    public Transform ObjectToTrack;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = new Vector3(0, FollowOffsetY, FollowOffsetZ);
+    }
 
     private void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, ObjectToTrack.position + offset, Time.deltaTime * LerpFactor);
     }
 }
