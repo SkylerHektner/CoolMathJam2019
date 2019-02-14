8b797f2ef89ebe39eb01e0684448a20b2da84d4b
Skyler Hektner <skylerhektner@gmail.com>
2019-02-14 00:30:34 -0700

    Added in basic possession and possession framework

---
+++ CoolMathJam/Assets/Scripts/Skeleton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour, IPossessable
+{
+    public Transform neckTransform;
+    public Material ValidCandidateMat;
+
+    private Renderer renderer;
+    private Material originalMat;
+
+    private void Start()
+    {
+        renderer = GetComponentInChildren<Renderer>();
+        originalMat = renderer.material;
+    }
+
+    public Transform getMountTransform()
+    {
+        return neckTransform;
+    }
+
+    public bool IsInRange(float range, Vector3 position)
+    {
+        return (transform.position - position).sqrMagnitude < range * range;
+    }
+
+    public void OnBecomeCandidate()
+    {
+        renderer.material = ValidCandidateMat;
+    }
+
+    public void OnDePossess()
+    {
+        renderer.material = ValidCandidateMat;
+    }
+
+    public void OnNoLongerCandidate()
+    {
+        renderer.material = originalMat;
+    }
+
+    public void OnPossess()
+    {
+        renderer.material = originalMat;
+    }
+}
