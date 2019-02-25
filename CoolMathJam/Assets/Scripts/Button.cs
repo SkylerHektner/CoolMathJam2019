e4ee7aa3461dc4aeb4b927c6cba692aa1b060f97
Skyler Hektner <skylerhektner@gmail.com>
2019-02-24 18:04:56 -0700

    Added in 6 level prefabs and some placeholder prefabs

---
+++ CoolMathJam/Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject[] Triggerables;
+    private List<ITriggerable> triggerables = new List<ITriggerable>();
+
+    private bool pressed = false;
+
+    private void Start()
+    {
+        foreach (GameObject g in Triggerables)
+        {
+            triggerables.Add(g.GetComponent<ITriggerable>());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!pressed && other.tag == "Skeleton" || other.tag == "Skull")
+        {
+            foreach (ITriggerable t in triggerables)
+            {
+                t.OnTriggerOn();
+            }
+            pressed = true;
+        }
+    }
+}
